7449947cc0e38f2f983ebffec5a35973dead251f
IrinaKostrba <kos22487@spengergasse.at>
2024-01-05 08:10:07 +0100

    aboutus grundgerüst und bilder eingefügt

--- api.js
+++ api.js
@@ -1,9 +1,53 @@
-const client = require('./connection.js')
+// api.js
 const express = require('express');
-const app = express();
+const router = express.Router();
+const client = require('./connection.js');
 
-app.listen(3300, () => {
-    console.log("Sever is now listening at port 3000");
-})
+// Middleware, um die Verbindung zu öffnen
+router.use((req, res, next) => {
+    client.connect();
+    next();
+});
 
-client.connect();
+router.get('/aerzte', (req, res) => {
+    client.query('SELECT * FROM a_aerzte', (err, result) => {
+        if (!err) {
+            res.send(result.rows);
+        } else {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+router.post('/register', (req, res) => {
+    const { email, password, confirmPassword, p_id } = req.body;
+
+    if (password !== confirmPassword) {
+        return res.status(400).json({ error: 'Passwords do not match' });
+    }
+
+    // Füge den Benutzer zur Datenbank hinzu, ohne u_id explizit anzugeben
+    const query = 'INSERT INTO u_userverwaltung (u_email, u_passwort, u_p_id) VALUES ($1, $2, $3) RETURNING *';
+    const values = [email, password, p_id]; // Verwende p_id für u_p_id
+
+    client.query(query, values, (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        res.status(201).json({ message: 'User registered successfully', user: result.rows[0] });
+    });
+});
+
+
+
+
+// Middleware, um die Verbindung zu schließen
+router.use((req, res, next) => {
+    client.end();
+    next();
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-// app.js - Main application file
+// app.js
 const express = require('express');
 const path = require('path');
-const db = require('./db'); // Import your database connection
+const client = require('./connection.js');
+const apiRouter = require('./api');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Statische Dateien im "public"-Ordner verfügbar machen
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routen-Handler für die Wurzel-URL
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
-// Beispiel: Eine Route für das Abrufen von Daten aus der Datenbank
-app.get('/getdata', (req, res) => {
-  // Hier kannst du Daten aus der Datenbank abrufen und sie als JSON zurückgeben
-  res.json({ message: 'Daten aus der Datenbank abgerufen' });
-});
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
   console.log(`App läuft auf Port ${port}`);
