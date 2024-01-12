0b71146afde59fce089514ff2811f58dc5679082
KIK22670 <kikicaleksandra@gmail.com>
2024-01-12 21:02:18 +0100

    Bewertung + Patiendaten

--- connection.js
+++ connection.js
@@ -1,15 +1,29 @@
-const { Client } = require('pg')
+const { Client } = require('pg');
 
-const client = new Client({
+let client;
+
+if (process.env.NODE_ENV === 'production') {
+  // Verbindung zur Heroku PostgreSQL-Datenbank
+  client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  // Lokale Verbindung
+  client = new Client({
     host: "localhost",
     user: "postgres",
     port: 5432,
     password: "rootUser",
     database: "postgres"
-})
+  });
+}
 
 client.connect()
   .then(() => console.log('Connected to the database'))
   .catch(error => console.error('Error connecting to the database', error));
 
 module.exports = client;
+
