fdd2dc19bfcbb75a71c8cd8ab97ebdb3b60b5848
KIK22670 <kikicaleksandra@gmail.com>
2024-01-14 17:14:45 +0100

    Revert "connection fix"
    This reverts commit bdb72fdbf52f870a811af4a9990f17fb3e11fe19.

--- connection.js
+++ connection.js
@@ -1,3 +1,5 @@
+const { Client } = require('pg');
+
 let client;
 
 if (process.env.NODE_ENV === 'production') {
@@ -18,3 +20,10 @@ if (process.env.NODE_ENV === 'production') {
     database: "postgres"
   });
 }
+
+client.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(error => console.error('Error connecting to the database', error));
+
+module.exports = client;
+
