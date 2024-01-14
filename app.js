96ec2830b4d32805cf9804130cd6806d8c4ede6a
KIK22670 <kikicaleksandra@gmail.com>
2024-01-14 16:28:26 +0100

    connection fixed 2

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+// Stelle sicher, dass client korrekt initialisiert wird
+const client = require('connection.js');
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const client = require('./connection.js');
 const apiRouter = require('./api');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
@@ -143,11 +144,6 @@ app.get('/logout', (req, res) => {
     }
   });
 });
-
-// ...
-
-
-
 
 
 
