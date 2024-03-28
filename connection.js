d519da2552b2f9f7de9df43be72e0332ae807999
KIK22670 <kikicaleksandra@gmail.com>
2024-03-28 12:13:13 +0100

    Revert "terminverwaltung (funktioniert nicht ganz)"
    This reverts commit b274ac9a738f4c966771df0099102e58b77559ec.

--- connection.js
+++ connection.js
@@ -27,39 +27,3 @@ client.connect()
 
 module.exports = client;
 
-const client = require('./db'); // Importieren Sie Ihre PostgreSQL-Client-Instanz
-
-// Funktion zum Abrufen aller Termine
-async function getTermine() {
-  try {
-    const result = await client.query('SELECT * FROM t_termine');
-    return result.rows;
-  } catch (error) {
-    console.error('Error fetching appointments', error);
-    throw error;
-  }
-}
-
-// Funktion zum Löschen eines Termins
-async function deleteTermin(terminId) {
-  try {
-    await client.query('DELETE FROM t_termine WHERE t_id = $1', [terminId]);
-  } catch (error) {
-    console.error('Error deleting appointment', error);
-    throw error;
-  }
-}
-
-// Funktion zum Bearbeiten eines Termins (falls erforderlich)
-async function editTermin(terminId, newData) {
-  try {
-    // Implementieren Sie die Logik zum Bearbeiten des Termins entsprechend Ihrer Anforderungen
-  } catch (error) {
-    console.error('Error editing appointment', error);
-    throw error;
-  }
-}
-
-module.exports = { getTermine, deleteTermin, editTermin };
-
-
